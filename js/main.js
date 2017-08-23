021cbf217f74a28a8ed32635b5f356463c73a1d3
Marcin Hagmajer <mhagmajer@gmail.com>
2017-08-23 20:29:16 +0200

    copy

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
         scrollSpeed: 1200
     });
 
-     
+
     //animated header class
     $(window).scroll(function () {
         if ($(window).scrollTop() > 100) {
@@ -26,17 +26,17 @@ $(document).ready(function(){
         }
     });
 
-    $('#countdown_dashboard').countDown({
-        targetDate: {
-            'day':      11,
-            'month':    3,
-            'year':     2017,
-            'hour':     00,
-            'min':      00,
-            'sec':      01,
-        },
-        omitWeeks: true
-    });
+    // $('#countdown_dashboard').countDown({
+    //     targetDate: {
+    //         'day':      11,
+    //         'month':    3,
+    //         'year':     2017,
+    //         'hour':     00,
+    //         'min':      00,
+    //         'sec':      01,
+    //     },
+    //     omitWeeks: true
+    // });
 
     $('.init-slider').owlCarousel({
         items:1,
@@ -108,36 +108,36 @@ $(document).ready(function(){
 });
 
 // When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 16,
-
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(23.751945, 90.384590), // Dhaka ,
-        scrollwheel: false,
-
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
-    };
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map-canvas');
-
-    // Create the Google Map using our element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Let's also add a marker while we're at it
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(23.751945, 90.384590),
-        map: map,
-        icon: 'img/map.png',
-        title: 'Twing!'
-    });
-}
+// google.maps.event.addDomListener(window, 'load', init);
+//
+// function init() {
+//     // Basic options for a simple Google Map
+//     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+//     var mapOptions = {
+//         // How zoomed in you want the map to start at (always required)
+//         zoom: 16,
+//
+//         // The latitude and longitude to center the map (always required)
+//         center: new google.maps.LatLng(23.751945, 90.384590), // Dhaka ,
+//         scrollwheel: false,
+//
+//         // How you would like to style the map.
+//         // This is where you would paste any style found on Snazzy Maps.
+//         styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
+//     };
+//
+//     // Get the HTML DOM element that will contain your map
+//     // We are using a div with id="map" seen below in the <body>
+//     var mapElement = document.getElementById('map-canvas');
+//
+//     // Create the Google Map using our element and options defined above
+//     var map = new google.maps.Map(mapElement, mapOptions);
+//
+//     // Let's also add a marker while we're at it
+//     var marker = new google.maps.Marker({
+//         position: new google.maps.LatLng(23.751945, 90.384590),
+//         map: map,
+//         icon: 'img/map.png',
+//         title: 'Twing!'
+//     });
+// }
